a5db84e3baa8419179188dca5ea094966a789e11
AmandaEmilia <amanda.e.strom@gmail.com>
2025-06-09 16:33:45 +0200

    structing and adding empty components for dashboard
    Co-authored-by: Ikbal Gundogdu <ikbalgun98@gmail.com>
    Co-authored-by: Elias Egelrud <elias.egelrud@gmail.com>

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
-import SideBar from '@/components/SideBar'
+import SideBar from '@/components/generic/SideBar'
 
 export const Route = createRootRoute({
   component: () => (
