d30ec9dc45ce12d3678de40849cca45e90d231b5
Elias.Egel <elias.egelrid@gmail.com>
2025-06-09 14:52:40 +0200

    removed header
    Co-authored-by: Amanda <amanda.e.strom@gmail.com>
    Co-authored-by: Ikbal <ikbalgun98@gmail.com>

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
-import Header from '../components/Header'
 import SideBar from '@/components/SideBar'
 
 export const Route = createRootRoute({
