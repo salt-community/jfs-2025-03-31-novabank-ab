4ac59e41e29ebb4a1a627ccb29a6ba1d4b2a91e3
Elias.Egel <elias.egelrid@gmail.com>
2025-06-09 15:18:45 +0200

    wip sidebar
    Co-authored-by: Amanda <amanda.e.strom@gmail.com>
    Co-authored-by: Ikbal <ikbalgun98@gmail.com>

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -2,13 +2,17 @@ import { Link } from '@tanstack/react-router'
 
 export default function SideBar() {
   return (
-    <nav className="fixed left-1 bg-amber-300">
-      <div className="px-2 font-bold">
+    <aside className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 bg-amber-300 text-center">
+      <div className="px-2 font-bold ">
         <Link to="/">Home</Link>
       </div>
-      <div className="px-2 font-bold">
-        <Link to="/">Home</Link>
+      <ul className="list-none content-center">
+        <li>Account</li>
+        <li>Transaction</li>
+      </ul>
+      <div className="px-2 font-bold content-end">
+        <Link to="/">Settings</Link>
       </div>
-    </nav>
+    </aside>
   )
 }
