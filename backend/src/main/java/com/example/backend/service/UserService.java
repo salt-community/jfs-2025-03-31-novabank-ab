8c7418f5fe798b2aa6cf02bae2228b43923031d7
samuel-kar <samuel.karlhager@hotmail.com>
2025-06-10 13:49:41 +0200

    begin to add logic to user service
    Co-authored-by: Nikita Mokhonko <nikita.mokhonko@appliedtechnology.se>
    Co-authored-by: Andreas Kamf <andreas.kamf@appliedtechnology.se>
    Co-authored-by: Daniel Sandström <daniel.sandstrom@appliedtechnology.se>

--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -4,6 +4,9 @@ import com.example.backend.model.User;
 import com.example.backend.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
 
@@ -17,4 +20,21 @@ public class UserService {
         return userRepository.save(user);
     }
 
+
+    public User getUser(long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElse(null);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void deleteUser(long id) {
+        userRepository.deleteById(id);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
 }
