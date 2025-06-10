3e89c160fb93db2fd3c6871708ab44382aa5fcdb
Baset Kazemi <basetkazemi10@gmail.com>
2025-06-10 18:03:04 +0200

    implemented scheduledRequestDto

--- backend/src/main/java/com/example/backend/dto/ScheduledRequestDto.java
+++ backend/src/main/java/com/example/backend/dto/ScheduledRequestDto.java
@@ -1,4 +1,15 @@
 package com.example.backend.dto;
 
-public record ScheduledRequestDto() {
+import com.example.backend.model.Account;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+public record ScheduledRequestDto(
+        Account from,
+        Account to,
+        @NotNull
+        double amount,
+        LocalDateTime date,
+        String description
+) {
 }
