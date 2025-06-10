8939502e971b421286ca9baccafe629c9060afa0
Andreas Kamf <andreaskamf@gmail.com>
2025-06-10 16:00:44 +0200

    added TODO

--- backend/src/main/java/com/example/backend/dto/CreateAccountRequestDto.java
+++ backend/src/main/java/com/example/backend/dto/CreateAccountRequestDto.java
@@ -1,9 +1,11 @@
 package com.example.backend.dto;
 
 import com.example.backend.model.Account;
+import com.example.backend.model.User;
 import jakarta.annotation.Nonnull;
 
 // TODO: Change currency to Currency
+// TODO: Implement toAccount
 public record CreateAccountRequestDto(
         @Nonnull Long userId,
         String currency
