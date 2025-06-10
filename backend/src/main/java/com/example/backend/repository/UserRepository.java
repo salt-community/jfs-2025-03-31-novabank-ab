41d2698b8f919ea748711b1e0b09f51d952d886d
Andreas Kamf <andreaskamf@gmail.com>
2025-06-10 11:17:58 +0200

    fixed Service!

--- backend/src/main/java/com/example/backend/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example.backend.repository;
 
-import com.example.backend.model.PlaceHolderUser;
+import com.example.backend.model.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<PlaceHolderUser, Long> {}
+public interface UserRepository extends CrudRepository<User, Long> {}
--- backend/src/main/java/com/example/backend/service/UserService.java
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.backend.service;
 
+import com.example.backend.model.User;
 import com.example.backend.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
