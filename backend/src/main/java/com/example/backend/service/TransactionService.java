52f0a58eb299518af17d4175530f068c6c44dc88
Baset Kazemi <basetkazemi10@gmail.com>
2025-06-10 15:54:42 +0200

    Implemented transaction service

--- backend/src/main/java/com/example/backend/service/TransactionService.java
+++ backend/src/main/java/com/example/backend/service/TransactionService.java
@@ -1,8 +1,11 @@
 package com.example.backend.service;
 
+import com.example.backend.model.Transaction;
 import com.example.backend.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransactionService {
@@ -12,6 +15,27 @@ public class TransactionService {
     @Autowired
     public TransactionService(TransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
+    }
+
+
+    public Transaction getTransaction() {
+        return null;
+    }
+
+    public void addTransaction() {
+
+    }
+
+    public void addScheduledTransaction() {
+
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return null;
+    }
+
+    public void deleteScheduledTransaction() {
+
     }
 
 
