bc597bed96d061e2b99d0c8c276f4691662eec07
Baset Kazemi <basetkazemi10@gmail.com>
2025-06-10 15:50:07 +0200

    Created Transaction service

---
+++ backend/src/main/java/com/example/backend/service/TransactionService.java
@@ -0,0 +1,18 @@
+package com.example.backend.service;
+
+import com.example.backend.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+
+}
