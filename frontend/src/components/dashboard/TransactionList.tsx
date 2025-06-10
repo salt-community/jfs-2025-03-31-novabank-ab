1a8c5234be38571ca9d47212268ee54f1e2a3192
Elias.Egel <elias.egelrid@gmail.com>
2025-06-10 15:53:37 +0200

    added start of transactionlist

---
+++ frontend/src/components/dashboard/TransactionList.tsx
@@ -0,0 +1,10 @@
+export default function TransactionList() {
+  return (
+    <>
+      <div className="flex justify-between items-center p-10">
+        <div>Recent transactions</div>
+        <div>see all</div>
+      </div>
+    </>
+  )
+}
