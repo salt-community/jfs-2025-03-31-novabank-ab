c5e23524627497f4f7c483298051160101e4dd51
Baset Kazemi <basetkazemi10@gmail.com>
2025-06-10 17:52:55 +0200

    Implemented TransactionRequestDto

--- backend/src/main/java/com/example/backend/controller/TransactionController.java
+++ backend/src/main/java/com/example/backend/controller/TransactionController.java
@@ -2,8 +2,12 @@ package com.example.backend.controller;
 
 import com.example.backend.dto.ScheduledRequestDto;
 import com.example.backend.dto.TransactionRequestDto;
+import com.example.backend.model.Account;
+import com.example.backend.model.Transaction;
+import com.example.backend.service.AccountService;
 import com.example.backend.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,10 +16,12 @@ import org.springframework.web.bind.annotation.*;
 public class TransactionController {
 
     private final TransactionService transactionService;
+    private final AccountService accountService;
 
     @Autowired
-    public TransactionController(TransactionService service){
+    public TransactionController(TransactionService service, AccountService accountService){
         this.transactionService = service;
+        this.accountService = accountService;
     }
 
 
@@ -33,12 +39,19 @@ public class TransactionController {
 
     @PostMapping
     public ResponseEntity<?> addTransaction( @RequestBody TransactionRequestDto dto){
-        transactionService.addTransaction(dto);
+        Account from = accountService.getAccount(dto.from().getId());
+        Account to = accountService.getAccount(dto.to().getId());
+        Transaction transaction = new Transaction();
+        transaction.setFromAccount(from);
+        transaction.setToAccount(to);
+        transaction.setAmount(dto.amount());
+        transactionService.addTransaction(transaction);
         return null;
     }
 
     @PostMapping("/scheduled")
     public ResponseEntity<?> addScheduledTransaction(@RequestBody ScheduledRequestDto dto){
+
         transactionService.addScheduledTransaction(dto);
         return null;
     }
--- backend/src/main/java/com/example/backend/dto/TransactionRequestDto.java
+++ backend/src/main/java/com/example/backend/dto/TransactionRequestDto.java
@@ -1,7 +1,7 @@
 package com.example.backend.dto;
 
 import com.example.backend.model.Account;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 public record TransactionRequestDto(Account from,
                                     Account to,
