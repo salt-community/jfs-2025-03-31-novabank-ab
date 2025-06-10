66acd3516da2404f660e6e3fcfa293b24c0bb6b1
Daniel Sandstrom <github@danielsandstrom.org>
2025-06-10 14:43:36 +0200

    Quick fix of BalanceRepository

--- backend/src/main/java/com/example/backend/repository/BalanceRepository.java
+++ backend/src/main/java/com/example/backend/repository/BalanceRepository.java
@@ -1,5 +1,6 @@
 package com.example.backend.repository;
 
+import com.example.backend.model.Balance;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BalanceRepository extends CrudRepository<PlaceHolderBalance, Long> {}
+public interface BalanceRepository extends CrudRepository<Balance, Long> {}
