841f277ec2a8184094effbe1cea4a12e5683a10a
Nikita <nikitamokhonko@gmail.com>
2025-06-11 14:04:10 +0200

    Restructured routes

--- frontend/src/routes/settings.tsx
+++ frontend/src/routes/settings.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router'
+import SideBar from '@/components/generic/SideBar'
+import SettingsPage from '@/pages/settingsPage/SettingsPage'
 
 export const Route = createFileRoute('/settings')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">Hello "/settings"!</div>
+  return(
+  <>
+  <SettingsPage />
+  </>
+  )
 }
