aa77f688ba8d40ae5d6f72992706b586cf01cf68
Andreas Kamf <andreaskamf@gmail.com>
2025-06-11 11:16:33 +0200

    init account-no generator

--- backend/src/main/java/com/example/backend/service/AccountService.java
+++ backend/src/main/java/com/example/backend/service/AccountService.java
@@ -11,6 +11,7 @@ import com.example.backend.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -55,5 +56,28 @@ public class AccountService {
 
     public Account createAccount(Account account) {
         return accountRepository.save(account);
+    }
+
+
+    private String generateUniqueAccountNumber() {
+        String prefix = "1337-";
+        String accountNumber;
+
+        do {
+            accountNumber = prefix + generateRandomDigits(9);
+        } while (accountRepository.existsByAccountNumber(accountNumber));
+
+        return accountNumber;
+    }
+
+    private String generateRandomDigits(int length) {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            sb.append(random.nextInt(10));
+        }
+
+        return sb.toString();
     }
 }
