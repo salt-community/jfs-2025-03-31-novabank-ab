d79773daae80432fdb325b53e3662e09c74bbc42
samuel-kar <samuel.karlhager@hotmail.com>
2025-06-11 14:49:20 +0200

    add dependency for webclient and constructor with builder

--- backend/src/main/java/com/example/backend/service/CurrencyService.java
+++ backend/src/main/java/com/example/backend/service/CurrencyService.java
@@ -1,12 +1,21 @@
 package com.example.backend.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class CurrencyService {
 
+    private final WebClient webClient;
     private final String API_URL = "";
     private final String API_KEY = "";
+
+    public CurrencyService() {
+        this.webClient = WebClient.builder()
+                .baseUrl("https://api.riksbank.se")
+                .defaultHeader("Ocp-Apim-Subscription-Key", API_KEY)
+                .build();
+    }
 
     public double fetchSekEurPmi() {
 
