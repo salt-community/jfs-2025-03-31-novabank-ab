785b12b395265d12a7fc7ac85f7cbb9295db347c
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-11 14:34:40 +0200

    update sidebar

--- frontend/src/components/dashboard/TransactionList.tsx
+++ frontend/src/components/dashboard/TransactionList.tsx
@@ -25,7 +25,7 @@ export default function TransactionList() {
   ];
 
   return (
-    <div className="p-10 rounded-lg max-w-md mx-auto"> 
+    <div className="p-10 rounded-lg max-w-xl mx-auto"> 
       <div className="flex justify-between items-center p-10"> 
         <h2 className="text-xl text-gray-800">Recent transactions</h2>
         <a href="#" className="text-sm text-gray-600 hover:text-gray-900">See all</a>
