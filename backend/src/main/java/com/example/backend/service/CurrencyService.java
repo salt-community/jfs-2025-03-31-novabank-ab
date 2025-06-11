c728dbb0c1bc4707d0f1c79ef0806d6b37db04e9
samuel-kar <samuel.karlhager@hotmail.com>
2025-06-11 14:29:14 +0200

    add new service for translating between currencies eur and sek

---
+++ backend/src/main/java/com/example/backend/service/CurrencyService.java
@@ -0,0 +1,17 @@
+package com.example.backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CurrencyService {
+
+    private final String API_URL = "";
+    private final String API_KEY = "";
+
+    public double fetchSekEurPmi() {
+
+    }
+
+
+
+}
