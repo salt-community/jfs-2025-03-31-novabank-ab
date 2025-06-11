1affddb101e89395612482cb54ffc61f943faa14
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-11 15:47:58 +0200

    give base design for every page

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,16 +1,20 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
+import { SignedIn } from '@clerk/clerk-react'
 
 import SideBar from '@/components/generic/SideBar'
 
 export const Route = createRootRoute({
   component: () => (
-    <>
-    <SignedIn>
-      <SideBar />
-    </SignedIn>
-      <Outlet />
-    </>
+    <div className="flex h-screen font-lato">
+      <SignedIn>
+        <aside className="w-1/5 h-screen">
+          <SideBar />
+        </aside>
+      </SignedIn>
+
+      <main className="flex-1 mx-20 my-15 h-full bg-white text-black">
+        <Outlet />
+      </main>
+    </div>
   ),
 })
