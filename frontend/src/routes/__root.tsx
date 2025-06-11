8a90c344638faa4a9caa25384d28a7747bd25e45
Elias.Egel <elias.egelrid@gmail.com>
2025-06-11 08:45:47 +0200

    made the import in alpabetical order

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import { SignedIn, SignedOut, UserButton, SignInButton } from '@clerk/clerk-react'
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
 
 import SideBar from '@/components/generic/SideBar'
 
