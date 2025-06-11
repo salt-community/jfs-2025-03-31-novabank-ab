a9c92deeb30ce74bf433b93633c1e3e3cb13d99e
AmandaEmilia <amanda.e.strom@gmail.com>
2025-06-11 17:42:09 +0200

    Fix for renaming folder

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,15 +1,20 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/clerk-react'
 
 import SideBar from '@/components/generic/SideBar'
 
 export const Route = createRootRoute({
   component: () => (
     <>
-    <SignedIn>
-      <SideBar />
-    </SignedIn>
+      <SignedIn>
+        <SideBar />
+      </SignedIn>
       <Outlet />
     </>
   ),
