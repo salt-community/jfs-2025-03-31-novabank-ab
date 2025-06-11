9bea231e15b37d52affc3be9c7f79a3bf006da11
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-11 15:46:32 +0200

    fixed linting error for deploying frontend
    Co-authored-by: Ludwig Hahn <00ludwighahn@gmail.com>
    Co-authored-by: David Aslan <david.aslan@appliedtechnology.se>

--- frontend/src/pages/dashboardPage/DashboardPage.tsx
+++ frontend/src/pages/dashboardPage/DashboardPage.tsx
@@ -1,6 +1,5 @@
 import TransactionList from '@/components/dashboard/TransactionList'
 import AccountGallery from '@/components/dashboard/AccountGallery'
-import SideBar from '@/components/generic/SideBar'
 
 export default function DashboardPage() {
   return (
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,8 +1,7 @@
-import { createFileRoute } from '@tanstack/react-router'
-import DashboardPage from '@/pages/dashboardPage/DashboardPage'
+import { SignInButton, SignedIn, SignedOut, useUser } from '@clerk/clerk-react'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import { useEffect } from 'react'
-import { useNavigate } from '@tanstack/react-router'
-import { SignedIn, SignedOut, SignInButton, useUser } from '@clerk/clerk-react'
+
 import Header from '@/components/generic/Header'
 
 export const Route = createFileRoute('/')({
@@ -25,24 +24,23 @@ function RedirectOnSignIn() {
 function LandingPage() {
   return (
     <>
-    <Header />
-    <div className="min-h-screen flex flex-col items-center justify-center bg-blue-500 text-white text-[calc(10px+2vmin)]">
-      <h1>Welcome to the Landing Page!</h1>
-      <p>Please sign in to continue.</p>
-      <div className="mx-auto mt-30 mb-25 flex justify-center items-center ">
-        <SignedOut>
-          <SignInButton>
-            <button className="text-center ml-2 bg-black mr-2 text-white hover:cursor-pointer px-4 pb-1 border-2 border-white rounded-2xl">
-              Sign In
-            </button>
-          </SignInButton>
-        </SignedOut>
-        <SignedIn>
-          <RedirectOnSignIn />
-        </SignedIn>
+      <Header />
+      <div className="min-h-screen flex flex-col items-center justify-center bg-blue-500 text-white text-[calc(10px+2vmin)]">
+        <h1>Welcome to the Landing Page!</h1>
+        <p>Please sign in to continue.</p>
+        <div className="mx-auto mt-30 mb-25 flex justify-center items-center ">
+          <SignedOut>
+            <SignInButton>
+              <button className="text-center ml-2 bg-black mr-2 text-white hover:cursor-pointer px-4 pb-1 border-2 border-white rounded-2xl">
+                Sign In
+              </button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <RedirectOnSignIn />
+          </SignedIn>
+        </div>
       </div>
-    </div>
-          
-      </>
+    </>
   )
 }
