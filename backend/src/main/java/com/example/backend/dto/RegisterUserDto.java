1f42143b344ebd256525a08cb9701807e92e23d9
Daniel Sandstrom <github@danielsandstrom.org>
2025-06-11 09:25:50 +0200

    Implemented RegisterUserDto fields

--- backend/src/main/java/com/example/backend/dto/RegisterUserDto.java
+++ backend/src/main/java/com/example/backend/dto/RegisterUserDto.java
@@ -1,10 +1,19 @@
 package com.example.backend.dto;
 
 import com.example.backend.model.User;
+import com.example.backend.model.enums.UserStatus;
 
-public record RegisterUserDto() {
+import java.time.LocalDateTime;
+
+public record RegisterUserDto(String fullname, String email, UserStatus status) {
 
     public User toUser() {
-        return new User();
+        User user = new User();
+        user.setFullName(fullname);
+        user.setEmail(email);
+        user.setStatus(status);
+        user.setCreatedAt(LocalDateTime.now());
+        user.setLastLogin(null);
+        return user;
     }
 }
