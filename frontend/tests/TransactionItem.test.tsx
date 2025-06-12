cb1935c3da2e3fcc4c0a22b5c9722c1a515a18d3
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-12 14:54:51 +0200

    verifying transaction item exists and displays correct time
    Co-authored-by: Ludwig Hahn <00ludwighahn@gmail.com>
    Co-authored-by: David Aslan <david.aslan@appliedtechnology.se>

---
+++ frontend/tests/TransactionItem.test.tsx
@@ -0,0 +1,39 @@
+import { describe, expect, it } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import TransactionItem from '../src/components/generic/TransactionItem'
+import '@testing-library/jest-dom'
+
+const testTransaction = {
+  name: 'Spendings Test',
+  category: 'Digital Game',
+  amount: 18.8888,
+  time: '18:03:1998',
+}
+
+describe('Transaction Item', () => {
+  it('should render a transaction item', () => {
+    render(
+      <TransactionItem
+        amount={testTransaction.amount}
+        category={testTransaction.category}
+        name={testTransaction.name}
+        time={testTransaction.time}
+      />,
+    )
+    const transactionItem = screen.getByTestId('transaction-item')
+    expect(transactionItem).toBeInTheDocument()
+  })
+  it('should verify the correct item time is displayed', () => {
+    render(
+      <TransactionItem
+        amount={testTransaction.amount}
+        category={testTransaction.category}
+        name={testTransaction.name}
+        time={testTransaction.time}
+      />,
+    )
+    const transactionItem = screen.getByTestId('transaction-item')
+    expect(transactionItem).toBeInTheDocument()
+    expect(transactionItem).toHaveTextContent(testTransaction.time)
+  })
+})
