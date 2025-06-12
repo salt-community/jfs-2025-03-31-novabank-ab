7f6ca58bfaa2600b95b3a84bacfb6ee8e1c82fbc
Elias.Egel <elias.egelrid@gmail.com>
2025-06-12 14:08:18 +0200

    Styling Transactionlist

--- frontend/src/components/dashboard/TransactionList.tsx
+++ frontend/src/components/dashboard/TransactionList.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "@tanstack/react-router";
 import TransactionItem from "../generic/TransactionItem";
 
 type Transaction={
@@ -13,12 +14,13 @@ type TransactionListProps={
 
 
 export default function TransactionList({transactions}: TransactionListProps) {
+  const navigate = useNavigate()
 
   return (
-    <div className="p-10 rounded-lg max-w-xl mx-auto"> 
-      <div className="flex justify-between items-center p-10"> 
-        <h2 className="text-xl text-gray-800">Recent transactions</h2>
-        <a href="#" className="text-sm text-gray-600 hover:text-gray-900">See all</a>
+    <div className="p-10 rounded-lg max-w mx-auto"> 
+      <div className="flex justify-between items-center"> 
+        <h2 className="text-xl text-gray-800 pb-5">Recent transactions</h2>
+        <a onClick={() => navigate({ to: '/transactions' })} className="text-sm text-gray-600 hover:text-gray-900 hover:cursor-pointer">See all</a>
       </div>
 
       <div className="shadow-md p-1">
