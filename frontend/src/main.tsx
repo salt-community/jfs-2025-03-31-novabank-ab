2d5420862d080115d289481f008b10228928d023
Elias.Egel <elias.egelrid@gmail.com>
2025-06-12 13:51:26 +0200

    tanstack fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ClerkProvider } from '@clerk/clerk-react'
 import "./i18n";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 
 
@@ -35,8 +34,6 @@ declare module '@tanstack/react-router' {
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-const queryClient = new QueryClient()
-
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
 }
@@ -52,7 +49,6 @@ if (rootElement && !rootElement.innerHTML) {
           <RouterProvider router={router} />
         </QueryClientProvider>
       </ClerkProvider>
-      </QueryClientProvider>
     </StrictMode>,
   )
 }
