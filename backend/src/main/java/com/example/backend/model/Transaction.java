5464e76982b9fefcae7673429519c3d782e2f1d9
Baset Kazemi <basetkazemi10@gmail.com>
2025-06-12 09:30:22 +0200

    Transaction entity

--- backend/src/main/java/com/example/backend/model/Transaction.java
+++ backend/src/main/java/com/example/backend/model/Transaction.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Setter
@@ -17,20 +18,19 @@ import java.time.LocalDateTime;
 public class Transaction {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private double amount;
-
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "from_account_id")
     private Account fromAccount;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "to_account_id")
     private Account toAccount;
-
     @Column(nullable = false)
     private LocalDateTime createdAt;
+    private double amount;
+    private String description;
+    private String userNote;
+    private String ocrNumber;
 
 }
