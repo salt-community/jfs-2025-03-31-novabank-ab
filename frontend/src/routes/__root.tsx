3bb41e352f5bbc12530f2f2246dbbd3856b2dfd2
Elias.Egel <elias.egelrid@gmail.com>
2025-06-12 14:02:55 +0200

    LandingPage fix

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { SignedIn } from '@clerk/clerk-react'
+import { SignedIn, SignedOut } from '@clerk/clerk-react'
 
 import SideBar from '@/components/generic/SideBar'
 
@@ -10,11 +10,16 @@ export const Route = createRootRoute({
         <aside className="w-1/5 h-screen">
           <SideBar />
         </aside>
+        <main className="flex-1 my-20 mx-30 h-full bg-white text-black">
+          <Outlet />
+        </main>
       </SignedIn>
 
-      <main className="flex-1 my-20 mx-30 h-full bg-white text-black">
-        <Outlet />
-      </main>
+      <SignedOut>
+        <main>
+          <Outlet />
+        </main>
+      </SignedOut>
     </div>
   ),
 })
