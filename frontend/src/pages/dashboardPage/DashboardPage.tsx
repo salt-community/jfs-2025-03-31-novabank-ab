cefa94b10cb934e519066f090c22fefb63f2afe9
AmandaEmilia <amanda.e.strom@gmail.com>
2025-06-12 10:44:56 +0200

    Add custom hook to fetch accounts + prep for real fetch to backend + dislaying accounts data in DashboardPage via AccountGallery

--- frontend/src/pages/dashboardPage/DashboardPage.tsx
+++ frontend/src/pages/dashboardPage/DashboardPage.tsx
@@ -1,28 +1,15 @@
+import { useAccounts } from '@/hooks'
 import TransactionList from '@/components/dashboard/TransactionList'
 import AccountGallery from '@/components/dashboard/AccountGallery'
 
 export default function DashboardPage() {
+  const { data, isLoading, isError } = useAccounts()
+
+  if (isLoading) return <p>Loading accounts...</p>
+  if (isError || !data) return <p>Failed to load accounts</p>
   return (
     <>
-      <AccountGallery
-        bankAccounts={[
-          {
-            name: 'Savings',
-            number: '**** 2201',
-            balance: 4465.23,
-          },
-          {
-            name: 'Personal',
-            number: '**** 7654',
-            balance: 532.78,
-          },
-          {
-            name: 'Family',
-            number: '**** 4720',
-            balance: 66004.65,
-          },
-        ]}
-      />
+      <AccountGallery bankAccounts={data} />
       <TransactionList />
     </>
   )
