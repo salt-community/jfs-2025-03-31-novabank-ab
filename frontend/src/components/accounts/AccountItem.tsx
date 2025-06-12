3b68578f21555ee696ccbd92b7456dde542fcc47
AmandaEmilia <amanda.e.strom@gmail.com>
2025-06-12 21:31:32 +0200

    Ref. extracted hardcoded data for AccountsPage

--- frontend/src/components/accounts/AccountItem.tsx
+++ frontend/src/components/accounts/AccountItem.tsx
@@ -1,38 +1,23 @@
-import { useNavigate } from '@tanstack/react-router';
-
-type Account = {
-  accountName: string
-  accountNumber: string
-  balance: number
-}
+import type { Account } from '@/types'
 
 type AccountItemProps = {
   account: Account
-};
+}
 
-export default function AccountItem({
-  account
-}: AccountItemProps) {
-  const navigate = useNavigate();
-
+export function AccountItem({ account }: AccountItemProps) {
   return (
-    <div
-      className="flex items-center justify-between border px-4 py-3 shadow-sm hover:bg-gray-200 rounded-md cursor-pointer"
-      onClick={() =>
-        navigate({
-          to: '/accounts/$id',
-          params: { id: account.accountNumber },
-        })
-      }
-    >
+    <div className="flex items-center justify-between border px-4 py-3 shadow-sm hover:bg-gray-200 rounded-md cursor-pointer">
       <div>
-        <div>{account.accountName}</div>
-        <div className="text-sm text-gray-500">{account.accountNumber}</div>
+        <div>{account.name}</div>
+        <div className="text-sm text-gray-500">{account.number}</div>
       </div>
       <div className="text-right">
-        ${account.balance.toLocaleString(undefined, { minimumFractionDigits: 2 })}
+        $
+        {account.balance.toLocaleString(undefined, {
+          minimumFractionDigits: 2,
+        })}
         <span className="ml-2">{'>'}</span>
       </div>
     </div>
-  );
+  )
 }
