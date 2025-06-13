84ff9e1ce0c28e6c5a7c263bdcdb7ece80988195
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-13 15:59:09 +0200

    remove hardcoded data

--- frontend/src/routes/transfer.tsx
+++ frontend/src/routes/transfer.tsx
@@ -1,30 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
 import TransferPage from '@/pages/transferPage/transferPage'
+import { useAccounts } from '@/hooks'
 
 export const Route = createFileRoute('/transfer')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return (
-    <TransferPage
-      bankAccounts={[
-        {
-          name: 'Savings',
-          number: '**** 2201',
-          balance: 4465.23,
-        },
-        {
-          name: 'Personal',
-          number: '**** 7654',
-          balance: 532.78,
-        },
-        {
-          name: 'Family',
-          number: '**** 4720',
-          balance: 66004.65,
-        },
-      ]}
-    />
-  )
+  const { data: accounts = [], isLoading, isError } = useAccounts()
+
+  if (isLoading) return <div className="p-8">Loading accounts...</div>
+
+  if (isError)
+    return <div className="p-8 text-red-500">Failed to load accounts</div>
+
+  return <TransferPage bankAccounts={accounts} />
 }
