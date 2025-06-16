08de7dd58e34da2582f9bac6cc8e96081fe5c7cd
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-16 14:15:37 +0200

    adding scheduled transactions icon

---
+++ frontend/src/assets/ThreeDotsIcon.tsx
@@ -0,0 +1,11 @@
+const ThreeDotsIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="bi bi-three-dots-vertical"
+    viewBox="0 0 16 16"
+    {...props}
+  >
+    <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+  </svg>
+)
+export default ThreeDotsIcon
--- frontend/src/components/generic/ScheduledTransactionItem.tsx
+++ frontend/src/components/generic/ScheduledTransactionItem.tsx
@@ -1,3 +1,4 @@
+import ThreeDotsIcon from '@/assets/ThreeDotsIcon'
 export type ScheduledTransactionItemProps = {
   fromAccountId: string
   toAccountId: string
@@ -22,13 +23,18 @@ export function ScheduledTransactionItem({
         <span className="text-base text-gray-800">{description}</span>
         <span className="text-xs text-gray-500">Note: {userNote}</span>
       </div>
-      <div className="flex flex-col items-end">
-        <span className={`text-base font-medium text-gray-800'`}>
-          {`-${Math.abs(amount).toFixed(2)}`}
-        </span>
-        <span className="text-xs text-gray-400">
-          {scheduledDate.split('T')[0]}
-        </span>
+      <div className="flex flex-row justify-center align-middle items-center">
+        <div className="flex flex-col items-end">
+          <span className={`text-base font-medium text-gray-800'`}>
+            {`-${Math.abs(amount).toFixed(2)}`}
+          </span>
+          <span className="text-xs text-gray-400">
+            {scheduledDate.split('T')[0]}
+          </span>
+        </div>
+        <div className="ml-4">
+          <ThreeDotsIcon width={24}></ThreeDotsIcon>
+        </div>
       </div>
     </div>
   )
