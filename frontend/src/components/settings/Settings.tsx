f83cc8a6ef3e55ce60e67081d7a31598a1a8497c
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-16 16:30:17 +0200

    basic tab component

--- frontend/src/components/settings/Settings.tsx
+++ frontend/src/components/settings/Settings.tsx
@@ -1,5 +1,28 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { useUser } from '@clerk/clerk-react'
 const Settings = () => {
-  return <div>Settings</div>
+  const { user } = useUser()
+  return (
+    <>
+      <Tabs defaultValue="personal">
+        <TabsList>
+          <TabsTrigger value="personal">Personal</TabsTrigger>
+          <TabsTrigger value="notifications">Notifications</TabsTrigger>
+          <TabsTrigger value="language">Language</TabsTrigger>
+        </TabsList>
+        <TabsContent value="personal">First name:</TabsContent>
+        <TabsContent value="notifications">notifications BRO</TabsContent>
+        <TabsContent value="language">language BRO</TabsContent>
+      </Tabs>
+      <button
+        onClick={() => {
+          console.log(user)
+        }}
+      >
+        TEST
+      </button>
+    </>
+  )
 }
 
 export default Settings
