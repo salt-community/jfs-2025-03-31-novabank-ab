4ecae3aad146bbbdde4a0c63b32288d95b9387d9
samuel-kar <samuel.karlhager@hotmail.com>
2025-06-17 11:01:16 +0200

    add api responses to endpoint

--- backend/src/main/java/com/example/backend/controller/CurrencyController.java
+++ backend/src/main/java/com/example/backend/controller/CurrencyController.java
@@ -4,6 +4,8 @@ import com.example.backend.dto.currencyDto.request.CurrencyConversionRequestDto;
 import com.example.backend.dto.currencyDto.response.CurrencyConversionResultDto;
 import com.example.backend.service.CurrencyService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +27,11 @@ public class CurrencyController {
                     "and returns the converted amount, together with other relevant data " +
                     "such as current rate and date.")
     @PostMapping("/convert")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully converted"),
+        @ApiResponse(responseCode = "400", description = "Invalid input or unsupported currency pair"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error - Unexpected Error")
+    })
     public ResponseEntity<CurrencyConversionResultDto> convert(@RequestBody CurrencyConversionRequestDto request) {
         CurrencyConversionResultDto result = currencyService.convertCurrency(request);
         return ResponseEntity.ok(result);
