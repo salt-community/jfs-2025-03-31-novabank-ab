053f14c58abb0ba18144edb0cd268917303d198a
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-17 09:44:11 +0200

    add username input

--- frontend/src/components/settings/Settings.tsx
+++ frontend/src/components/settings/Settings.tsx
@@ -10,17 +10,19 @@ const Settings = () => {
           <TabsTrigger value="notifications">Notifications</TabsTrigger>
           <TabsTrigger value="language">Language</TabsTrigger>
         </TabsList>
-        <TabsContent value="personal">First name:</TabsContent>
+        <div className="ml-5 mt-5">
+          <TabsContent value="personal">
+            <h3 className="text-xl mb-2">First name:</h3>
+            <input
+              className="bg-gray-200 rounded-xs p-1 w-[10vw]"
+              type="text"
+              value={user?.firstName ?? ''}
+            ></input>
+          </TabsContent>
+        </div>
         <TabsContent value="notifications">notifications BRO</TabsContent>
         <TabsContent value="language">language BRO</TabsContent>
       </Tabs>
-      <button
-        onClick={() => {
-          console.log(user)
-        }}
-      >
-        TEST
-      </button>
     </>
   )
 }
