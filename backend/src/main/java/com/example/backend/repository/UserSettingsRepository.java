dd365526064aa345d7971ace7b993dfb5fede6d2
Andreas Kamf <andreaskamf@gmail.com>
2025-06-17 13:08:51 +0200

    usersettings

---
+++ backend/src/main/java/com/example/backend/repository/UserSettingsRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend.repository;
+
+import com.example.backend.model.UserSettingsConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserSettingsRepository extends JpaRepository<UserSettingsConfig, UUID> {
+}
