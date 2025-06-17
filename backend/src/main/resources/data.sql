f31e969b4e9d7c669d5b30977b0db13a85593089
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-17 14:37:52 +0200

    bringing back old datasql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,53 +1,40 @@
--- Insert User
-INSERT INTO users (
-    id, password, first_name, last_name, email, phone_number, role, status, created_at, last_login
-) VALUES (
-             'user_2yMZ4k4VMcQ4C3Yowv3JfXg0mOd', 'encoded-password', 'John', 'Doe',
-             'john.doe@example.com', '0701234567', 'user', 'ACTIVE',
-             '2024-01-01T10:00:00', '2024-01-02T12:00:00'
-         );
+-- Currencies
+INSERT INTO currencies (id, name, abbrevation) VALUES
+    ('9bdc18e7-8173-4191-8f61-f3451c5e6759', 'Swedish Krona', 'SEK'),
+    ('9fa9391c-b25a-4933-8c58-e9c7c77f3620', 'Euro', 'EUR');
 
--- Insert Currency
-INSERT INTO currencies (id, name, abbrevation) VALUES (
-                                                          '1eccc3ba-d76c-4a97-9a10-c3b02d705555', 'Swedish Krona', 'SEK'
-                                                      );
+-- Users
+INSERT INTO users (id, full_name, email, phone_number, role, status, created_at, last_login) VALUES
+    ('user_2yMYqxXhoEDq64tfBlelGADfdlp', 'Amanda', 'amanda.strom@appliedtechnology.se', '0700000000', 'USER', 'ACTIVE', '2025-06-12 08:42:57', '2025-06-12 08:42:57'),
+    ('user_2ya9D2bPCC3XYyGsklZRQPPBeb9', 'Elias', 'elias.egelrud@appliedtechnology.se', '0700000001', 'USER', 'ACTIVE', '2025-06-12 08:42:57', '2025-06-12 08:42:57'),
+    ('b29f9397-ec6b-4d5e-9f1c-2939d2c1995b', 'User 3', 'user3@example.com', '0700000002', 'USER', 'ACTIVE', '2025-06-12 08:42:57', '2025-06-12 08:42:57');
 
--- Insert Account
-INSERT INTO accounts (
-    id, user_id, currency_id, created_at, balance, type, status, account_number
-) VALUES (
-             '11111111-1111-1111-1111-111111111111', 'user_2yMZ4k4VMcQ4C3Yowv3JfXg0mOd',
-             '1eccc3ba-d76c-4a97-9a10-c3b02d705555', '2024-01-01', 5000.00,
-             'PERSONAL', 'ACTIVE', '1234567890'
-         );
+    -- Accounts
+INSERT INTO accounts (id, user_id, currency_id, created_at, balance, type, status, account_number) VALUES
+    ('c1b35a93-9671-43c1-a0fe-aa4c2e9d2aec', 'user_2yMYqxXhoEDq64tfBlelGADfdlp', '9bdc18e7-8173-4191-8f61-f3451c5e6759', '2025-06-12', 9370, 'PERSONAL', 'ACTIVE', 'ACC1000'),
+    ('3587b71d-5845-4ba3-b667-e556bdf845be', 'user_2ya9D2bPCC3XYyGsklZRQPPBeb9', '9fa9391c-b25a-4933-8c58-e9c7c77f3620', '2025-06-12', 1763, 'PERSONAL', 'ACTIVE', 'ACC1001'),
+    ('7915a5bd-ddcc-4a40-8343-dee3a2188cf0', 'b29f9397-ec6b-4d5e-9f1c-2939d2c1995b', '9bdc18e7-8173-4191-8f61-f3451c5e6759', '2025-06-12', 7459, 'PERSONAL', 'ACTIVE', 'ACC1002'),
+    ('67d254c1-1532-4cf4-8d4f-55ef8fe12717', 'user_2yMYqxXhoEDq64tfBlelGADfdlp', '9fa9391c-b25a-4933-8c58-e9c7c77f3620', '2025-06-12', 2583, 'PERSONAL', 'ACTIVE', 'ACC1003'),
+    ('57a34877-b5f7-4967-a40b-c5cfba12cc67', 'user_2ya9D2bPCC3XYyGsklZRQPPBeb9', '9bdc18e7-8173-4191-8f61-f3451c5e6759', '2025-06-12', 9515, 'PERSONAL', 'ACTIVE', 'ACC1004'),
+    ('4cab58e6-5ebd-4cc3-b42c-1eca8da00a4a', 'b29f9397-ec6b-4d5e-9f1c-2939d2c1995b', '9fa9391c-b25a-4933-8c58-e9c7c77f3620', '2025-06-12', 4064, 'PERSONAL', 'ACTIVE', 'ACC1005'),
+    ('ec70e8d7-6fb5-4364-81da-9b1a52cc1650', 'user_2yMYqxXhoEDq64tfBlelGADfdlp', '9bdc18e7-8173-4191-8f61-f3451c5e6759', '2025-06-12', 2333, 'PERSONAL', 'ACTIVE', 'ACC1006'),
+    ('400086d4-7b9c-4052-abf3-651376b6817d', 'user_2ya9D2bPCC3XYyGsklZRQPPBeb9', '9fa9391c-b25a-4933-8c58-e9c7c77f3620', '2025-06-12', 9588, 'PERSONAL', 'ACTIVE', 'ACC1007');
 
--- Insert Nickname for Account
-INSERT INTO account_nickname (id, nickname, account_id) VALUES (
-                                                                   '22222222-2222-2222-2222-222222222222', 'Main Savings',
-                                                                   '11111111-1111-1111-1111-111111111111'
-                                                               );
+-- Transactions
+INSERT INTO transactions (id, from_account_id, to_account_id, created_at, amount, description, user_note, ocr_number) VALUES
+    ('2fcbbb39-06f2-4d67-a054-2da86129ee28', 'c1b35a93-9671-43c1-a0fe-aa4c2e9d2aec', '3587b71d-5845-4ba3-b667-e556bdf845be', '2025-06-12 08:42:57', 188, 'Description 0', 'Note 0', 'OCR00000'),
+    ('fa70865e-e018-410a-99a1-018ea90a3ce6', '3587b71d-5845-4ba3-b667-e556bdf845be', '7915a5bd-ddcc-4a40-8343-dee3a2188cf0', '2025-06-12 08:42:57', 632, 'Description 1', 'Note 1', 'OCR00001'),
+    ('62e1632f-e075-4add-984f-a3a7787efb83', '7915a5bd-ddcc-4a40-8343-dee3a2188cf0', '67d254c1-1532-4cf4-8d4f-55ef8fe12717', '2025-06-12 08:42:57', 670, 'Description 2', 'Note 2', 'OCR00002'),
+    ('a66ea65f-1a05-4153-bdb7-085ff312d6e6', '67d254c1-1532-4cf4-8d4f-55ef8fe12717', '57a34877-b5f7-4967-a40b-c5cfba12cc67', '2025-06-12 08:42:57', 354, 'Description 3', 'Note 3', 'OCR00003'),
+    ('a923eb88-eb4c-43f8-aed4-d3504ecce1e7', '57a34877-b5f7-4967-a40b-c5cfba12cc67', 'c1b35a93-9671-43c1-a0fe-aa4c2e9d2aec', '2025-06-12 08:42:57', 834, 'Description 4', 'Note 4', 'OCR00004'),
+    ('8d83bef8-c4de-4c3c-aaee-43df053b541b', 'c1b35a93-9671-43c1-a0fe-aa4c2e9d2aec', '3587b71d-5845-4ba3-b667-e556bdf845be', '2025-06-12 08:42:57', 741, 'Description 5', 'Note 5', 'OCR00005'),
+    ('4b49b008-71ff-4770-8417-f411e78f9283', '3587b71d-5845-4ba3-b667-e556bdf845be', '7915a5bd-ddcc-4a40-8343-dee3a2188cf0', '2025-06-12 08:42:57', 113, 'Description 6', 'Note 6', 'OCR00006'),
+    ('122f8446-0195-49db-9c83-18bb930bf18f', '7915a5bd-ddcc-4a40-8343-dee3a2188cf0', '67d254c1-1532-4cf4-8d4f-55ef8fe12717', '2025-06-12 08:42:57', 108, 'Description 7', 'Note 7', 'OCR00007'),
+    ('809cc380-badd-4876-b0ae-a8ff19f5c500', '67d254c1-1532-4cf4-8d4f-55ef8fe12717', '57a34877-b5f7-4967-a40b-c5cfba12cc67', '2025-06-12 08:42:57', 401, 'Description 8', 'Note 8', 'OCR00008'),
+    ('47271bd9-b2f5-458e-85b3-7d0ea5a7be7b', '57a34877-b5f7-4967-a40b-c5cfba12cc67', 'c1b35a93-9671-43c1-a0fe-aa4c2e9d2aec', '2025-06-12 08:42:57', 986, 'Description 9', 'Note 9', 'OCR00009');
 
--- Insert Transaction
-INSERT INTO transactions (
-    id, from_account_id, to_account_id, recipient_number, type, created_at,
-    amount, description, user_note, ocr_number
-) VALUES (
-             '33333333-3333-3333-3333-333333333333',
-             '11111111-1111-1111-1111-111111111111',
-             '11111111-1111-1111-1111-111111111111',
-             '9876543210', 1, '2024-01-10T14:30:00',
-             1000.00, 'Rent payment', 'January rent', 'OCR123456'
-         );
-
--- Insert Client (external)
-INSERT INTO clients (id, account_number) VALUES (
-                                                    '44444444-4444-4444-4444-444444444444', '9988776655'
-                                                );
-
--- Insert Application
-INSERT INTO applications (
-    id, created_at, status, first_name, last_name, personal_number, email, phone_number
-) VALUES (
-             '55555555-5555-5555-5555-555555555555', '2024-01-03T08:00:00', 'PENDING',
-             'Alice', 'Smith', '9001011234', 'alice@example.com', '0707654321'
-         );
+-- Scheduled Transactions
+INSERT INTO scheduled_transactions (id, from_account_id, to_account_id, amount, scheduled_date, status, created_at, ocr_number, user_note, description) VALUES
+    ('69ca9a09-804d-4c67-9227-ea23569d4a31', 'c1b35a93-9671-43c1-a0fe-aa4c2e9d2aec', '3587b71d-5845-4ba3-b667-e556bdf845be', 1331, '2025-06-19 08:42:57', 'PENDING', '2025-06-12 08:42:57', 'OCRS00000', 'Scheduled Note 0', 'Scheduled Description 0'),
+    ('0e683c21-5ed5-4498-b7d4-a9d4e432be02', '3587b71d-5845-4ba3-b667-e556bdf845be', '7915a5bd-ddcc-4a40-8343-dee3a2188cf0', 1204, '2025-06-19 08:42:57', 'PENDING', '2025-06-12 08:42:57', 'OCRS00001', 'Scheduled Note 1', 'Scheduled Description 1'),
+    ('4df15be9-12a2-4ee3-8f5c-c11dee6bae57', '7915a5bd-ddcc-4a40-8343-dee3a2188cf0', '67d254c1-1532-4cf4-8d4f-55ef8fe12717', 979, '2025-06-19 08:42:57', 'PENDING', '2025-06-12 08:42:57', 'OCRS00002', 'Scheduled Note 2', 'Scheduled Description 2');
