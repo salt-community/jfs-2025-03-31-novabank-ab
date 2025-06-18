464de221729aceb831ca4d525546bb32b25e6f4b
AmandaEmilia <amanda.e.strom@gmail.com>
2025-06-18 10:11:06 +0200

    Impl. createNewAccount

--- frontend/src/hooks/useCreateAccount.tsx
+++ frontend/src/hooks/useCreateAccount.tsx
@@ -7,7 +7,7 @@ export function useCreateAccount() {
   const queryClient = useQueryClient()
 
   return useMutation({
-    mutationFn: async (data: { type: string; abbreviation: string }) => {
+    mutationFn: async (data: { type: string; abbrevation: string }) => {
       const token = await getToken()
       if (!token) throw new Error('No auth token found')
       return createAccount(data, token)
