9dff6d573eb0b03639efea1c10eb4d62db2ab2a0
Luwig Hahn <00ludwighahn@gmail.com>
2025-06-18 14:56:22 +0200

    add admin/users route

--- frontend/src/routes/admin/users.tsx
+++ frontend/src/routes/admin/users.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import AdminUsersPage from '@/components/admin/users/AdminUsersPage'
 
 export const Route = createFileRoute('/admin/users')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/admin/users"!</div>
+  return <AdminUsersPage />
 }
