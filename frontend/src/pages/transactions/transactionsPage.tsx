00f1f3daa565fe9ff9ee35f7ff3e3baa3450f4c1
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-18 13:36:24 +0200

    translating transactions

--- frontend/src/pages/transactions/transactionsPage.tsx
+++ frontend/src/pages/transactions/transactionsPage.tsx
@@ -1,22 +1,30 @@
+import { useTranslation } from 'react-i18next'
 import type { Transaction } from '@/types'
 import { TransactionItem } from '@/components/generic'
 import { useGetAllTransactions } from '@/hooks'
 import Spinner from '@/components/generic/Spinner'
 
 export default function TransactionsPage() {
-  const { data: transactions = [], isLoading, isError } = useGetAllTransactions()
- 
+  const { t } = useTranslation('accounts')
+  const {
+    data: transactions = [],
+    isLoading,
+    isError,
+  } = useGetAllTransactions()
+
   if (isLoading) return <Spinner />
   if (isError) {
-    return <div className="p-8 text-red-500">Failed to load transactions</div>
+    return (
+      <div className="p-8 text-red-500">{t('failedToLoadTransactions')}</div>
+    )
   }
 
   return (
     <div>
-      <h1 className="text-3xl mb-20">All transactions</h1>
+      <h1 className="text-3xl mb-20">{t('allTransactions')}</h1>
       <div className=" px-5  shadow-sm">
         {transactions.length === 0 ? (
-          <div className="p-4 text-gray-500">No transactions found</div>
+          <div className="p-4 text-gray-500">{t('noTransactionsFound')}</div>
         ) : (
           transactions.map((tx: Transaction) => (
             <TransactionItem
