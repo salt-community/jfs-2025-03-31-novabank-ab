f0132d1323a926bdc2664ea7c90fbefb26dba7d5
Baset Kazemi <basetkazemi10@gmail.com>
2025-06-18 16:19:28 +0200

    refactor: improve Transaction entity annotations and structure for clarity

--- backend/src/main/java/com/example/backend/model/Transaction.java
+++ backend/src/main/java/com/example/backend/model/Transaction.java
@@ -3,37 +3,32 @@ package com.example.backend.model;
 import com.example.backend.model.enums.PaymentType;
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Entity
-@Table(name = "transactions")
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Entity
+@Table(name = "transactions")
 public class Transaction {
-
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @NonNull
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "from_account_id")
+    @JoinColumn(name = "from_account_id", nullable = false)
     private Account fromAccount;
 
-    @NonNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "to_account_id")
     private Account toAccount;
 
-    @Column
     private String recipientNumber;
 
-    @NonNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private PaymentType type;
@@ -41,19 +36,15 @@ public class Transaction {
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
-    @NonNull
     @Column(nullable = false)
     private double amount;
 
-    @NonNull
     @Column(nullable = false)
     private String description;
 
-    @NonNull
     @Column(nullable = false)
     private String userNote;
 
-    @NonNull
     @Column(nullable = false)
     private String ocrNumber;
 
@@ -61,5 +52,5 @@ public class Transaction {
     public void prePersist() {
         this.createdAt = LocalDateTime.now();
     }
-
 }
+
