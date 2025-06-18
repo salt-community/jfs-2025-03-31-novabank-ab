09c53f2d21b6ed08902a66b72af780729983b6c7
Philip Mokhonko <nikitamokhonko@gmail.com>
2025-06-18 11:40:57 +0200

    Fetching real transactions for individual accounts and sorting them by scheduled or done

--- frontend/src/components/generic/NoTransactionItem.tsx
+++ frontend/src/components/generic/NoTransactionItem.tsx
@@ -3,7 +3,7 @@ export function NoTransactionItem() {
     <div className="flex justify-between items-center py-3 border-b last:border-b-0">
       <div className="flex flex-col">
         <span className="text-sm text-gray-500">
-          No scheduled transactions found
+          No transactions found
         </span>
       </div>
     </div>
