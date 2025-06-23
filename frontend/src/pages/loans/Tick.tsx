84332267e78ceed57490cb126971f09a4a7b8301
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-06-23 15:44:01 +0200

    loans page

---
+++ frontend/src/pages/loans/Tick.tsx
@@ -0,0 +1,17 @@
+const Tick: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 -0.5 25 25"
+    {...props}
+  >
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="m5.5 12.5 4.667 4.5L19.5 8"
+    />
+  </svg>
+)
+export default Tick
