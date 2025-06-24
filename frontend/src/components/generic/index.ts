a764a41491e711d1228cf24bbea30ccfd6204f97
Ikbal Gundogdu <ikbalgun98@gmail.com>
2025-06-24 15:05:32 +0200

    refactor

--- frontend/src/components/generic/index.ts
+++ frontend/src/components/generic/index.ts
@@ -6,5 +6,5 @@ export * from './UserTopNav'
 export * from './UserBottomNav'
 export * from './Spinner'
 
-export * from './transaction-items/TransactionItem'
-export * from './transaction-items/ScheduledTransactionItem'
+export * from './transaction-items/Transactiontem'
+export * from './transaction-items/TransactionMenu'
