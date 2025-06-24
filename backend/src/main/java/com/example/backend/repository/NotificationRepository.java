08346a3100f8c837dec5ad11d96ee361662765a6
Andreas Kamf <andreaskamf@gmail.com>
2025-06-24 16:12:24 +0200

    added NotificationRepo

---
+++ backend/src/main/java/com/example/backend/repository/NotificationRepository.java
@@ -0,0 +1,11 @@
+package com.example.backend.repository;
+
+import com.example.backend.model.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+    List<Notification> findAllByUserIdOrderByCreatedAtDesc(String userId);
+}
